ed03433ea4ea92b583144582cb7278d8eade5743
himanshu sharma <sharmahimanshu6478@gmail.com>
2024-06-16 00:56:50 +0530

    file structure setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
-// <!-- https://www.youtube.com/watch?v=fUXQXafPF1A link for learn how to set tailwind in react + vite enviorment */ -->
+import { AlignJustify } from 'lucide-react';
+import React from 'react'
 
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-// import 'tailwindcss/tailwind.css'; 
-import './index.css'// Import Tailwind CSS first
-// import './App/css'; // Then, import your custom CSS file
-
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <>
-      <div className="p-4 w-full h-full bg-red-700">
-        <h1 className="text-5xl font-bold text-gray-800">Hello, Tailwind CSS!</h1>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Button
-        </button>
-      </div>
-    </>
+    <div>
+      this is app component
+      <AlignJustify />
+    </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/pages/Skill.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Skill = () => {
+  return (
+    <div>
+      this is skill
+    </div>
+  )
+}
+
+export default Skill
