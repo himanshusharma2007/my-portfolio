b07bb504093a1459db622cb7c713ae9a23d0e780
himanshu sharma <sharmahimanshu6478@gmail.com>
2024-06-19 22:00:19 +0530

    about page created

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,164 @@
-import React from 'react'
+import React from "react";
+import { BiPhone } from "react-icons/bi";
+import { GoArrowRight } from "react-icons/go";
+import { TfiEmail } from "react-icons/tfi";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
-    <div className="min-h-[88vh] bg--900 relative ">
-      
-    </div>
+    <section
+      id="about"
+      className="min-h-[88vh] bg-zinc-900 text-white flex  items-center"
+    >
+      <div>
+        <div className="wraper w-full flex justify-center items-center ">
+          <h2 className="text-4xl w-fit flex flex-col justify-center items-start font-bold text-center mb-6 ubuntu ">
+            About Me
+            <span className="h-[4px] w-[50px] bg-green-500 mt-1 rounded-md"></span>
+          </h2>
+        </div>
+        <div className="main w-full flex flex-col  md:flex-row  justify-between items-stretch">
+          <div className="left-container w-full md:w-[45%]  p-3 ubuntu-text flex flex-col justify-between">
+            <p className="mb-4 text-lg tracking-wide">
+              Hey there! I'm <span className="font-bold">Himanshu Sharma</span>,
+              a Full Stack Developer in my 3rd year of BCA at CIITM College. I
+              whip up fancy digital experiences and dabble in all kinds of
+              coding languages and tech.
+            </p>
+            <p className="mb-4 text-lg  tracking-wide">
+              I love cooking up creative solutions for both front-end and
+              back-end. I've mastered a bunch of tech and finished some pretty
+              cool projects. Let's create something awesome together!
+            </p>
+            <Link to="/skill">
+              <button className="bg-green-700 rounded-md text-md flex items-center p-2 space-x-2 max-w-fit text-center">
+                <div>My Skills</div> <GoArrowRight fontSize="20px" />
+              </button>
+            </Link>
+          </div>
+          <div className="right-contailer p-3 md:w-1/2 ">
+            <div className=" w-full  bg-zinc-800  overflow-y-scroll rounded-md p-3 custom-scrollbar flex flex-col space-y-5 max-h-[70vh] shadow-md">
+              <div className="Exprience">
+                <h2 className="flex text-xl justify-start items-center">
+                  <span className="bg-green-500 w-2 h-2 rounded-full mr-2"></span>
+                  Experience
+                </h2>
+                <div className="main w-full bg-zinc-700 p-3 mt-2 flex flex-col rounded-md">
+                  <div className="header w-full flex justify-between ">
+                    <h3 className="w-1/2">Full Stack dev Internship</h3>
+                    <h3 className="w-1/2 text-green-500 flex justify-end">
+                      Summer 2024
+                    </h3>
+                  </div>
+                  <div className="text-sm mt-3 ubuntu-text text-zinc-300">
+                    I took the full stack development internship organized by my
+                    college in the summer of 2024.
+                  </div>
+                </div>
+              </div>
+              <div className="Education">
+                <h2 className="flex text-xl justify-start items-center">
+                  <span className="bg-green-500 w-2 h-2 rounded-full mr-2"></span>
+                  Education
+                </h2>
+                <div className="main w-full bg-zinc-700 p-3 mt-2 flex flex-col rounded-md">
+                  <div className="header w-full flex justify-between ">
+                    <h3 className="w-1/2">Bechlor of Computer Applications</h3>
+                    <h3 className="w-1/2 text-green-500 flex justify-end">
+                      2022-2025
+                    </h3>
+                  </div>
+                  <div className="text-sm mt-3 ubuntu-text text-zinc-300">
+                    I'm pursing BCA 3rd year from CIITM college Jaipur
+                    ,Rajasthan.
+                  </div>
+                </div>
+              </div>
+              <div className="Certifications flex flex-col space-y-3">
+                <h2 className="flex text-xl justify-start items-center">
+                  <span className="bg-green-500 w-2 h-2 rounded-full mr-2"></span>
+                  Certifications
+                </h2>
+                <div className="certificate1 w-full bg-zinc-700 p-3 mt-2 flex flex-col rounded-md">
+                  <div className="header w-full flex justify-between ">
+                    <h3 className="w-full">
+                      Certification in Cyber Security given by Poblo Alto
+                      Networks{" "}
+                    </h3>
+                  </div>
+                  <div className="text-sm mt-3 ubuntu-text text-zinc-300">
+                    In this certification, I gained knowledge about web app
+                    security and protection. It equipped me with the skills to
+                    safeguard applications against various threats
+                  </div>
+                </div>
+                <div className="certificate2 w-full bg-zinc-700 p-3 mt-2 flex flex-col rounded-md">
+                  <div className="header w-full flex justify-between ">
+                    <h3 className="w-full">
+                      Introduction of MongoDB for Students{" "}
+                    </h3>
+                  </div>
+                  <div className="text-sm mt-3 ubuntu-text text-zinc-300">
+                    In this certification, I gained knowledge about the
+                    fundaments and some advance feature of MongoDB Database.
+                  </div>
+                </div>
+              </div>
+              <div className="Languges">
+                <h2 className="flex text-xl justify-start items-center">
+                  <span className="bg-green-500 w-2 h-2 rounded-full mr-2"></span>
+                  Language
+                </h2>
+                <div className="flex flex-col space-y-3">
+                  <div className="first w-full bg-zinc-700 p-3 mt-2 flex flex-col rounded-md">
+                    English
+                  </div>
+                  <div className="second w-full bg-zinc-700 p-3 mt-2 flex flex-col rounded-md">
+                    Hindi
+                  </div>
+                </div>
+              </div>
+              <div className="locality">
+                <h2 className="flex text-xl justify-start items-center">
+                  <span className="bg-green-500 w-2 h-2 rounded-full mr-2"></span>
+                  Locality
+                </h2>
+                <div className="wraper flex justify-between w-full bg-zinc-700 p-3 mt-2   rounded-md">
+                  Jaipur, Rajasthan
+                  <div>India</div>
+                </div>
+              </div>
+              <div className="freelancing">
+                <h2 className="flex text-xl justify-start items-center">
+                  <span className="bg-green-500 w-2 h-2 rounded-full mr-2"></span>
+                  Freelace/Remote work/Offline
+                </h2>
+                <div className=" w-full bg-zinc-700 p-3 mt-2   rounded-md">
+                  Available
+                </div>
+              </div>
+              <div className="contact">
+                <h2 className="flex text-xl justify-start items-center">
+                  <span className="bg-green-500 w-2 h-2 rounded-full mr-2"></span>
+                  Contact
+                </h2>
+                <div className="w-full bg-zinc-700 p-3 mt-2   rounded-md flex flex-col space-y-2">
+                  <div className="phone flex items-center space-x-2">
+                    <BiPhone className="text-[20px] " />{" "}
+                    <div> +91 9216363747</div>
+                  </div>
+                  <div className="email ml-1 flex items-center space-x-3">
+                    <TfiEmail className="text-[16px] text-center" />{" "}
+                    <div> sharmahimanshu6478@gmail.com</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
-}
+};
 
-export default About
+export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,6 +11,7 @@ import { gsap } from "gsap";
 import { useEffect, useRef } from "react";
 import { TextPlugin } from "gsap/all";
 import PhotoBorder from "../components/PhotoBorder";
+import About from "./About";
 
 
 const Home = () => {
@@ -58,103 +59,106 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="text-white min-h-[88vh] flex flex-col items-center justify-center ubuntu-text px-4">
-      <main className="flex flex-col-reverse sm:flex-col md:flex-row items-center justify-between w-full max-w-6xl">
-        <div className="text-center md:text-left md:w-1/2 space-y-3">
-          <h2 className="text-xl">Full Stack Developer</h2>
-          <h1 className="text-[28px] sm:text-[37px] font-bold ubuntu">
-            Hello I'm <br></br>
-            <div
-              ref={textRef}
-              className="text-green-500 min-h-16 h-auto  w-full"
-            >
-              {" "}
-            </div>
-          </h1>
-          <p className="text-zinc-400 text-wrap">
-            I excel at crafting elegant digital experiences and I am proficient
-            in various programming languages and technologies.
-          </p>
-          <div className="flex flex-col space-y-6 justify-center md:justify-start">
-            <div className="wrapper w-full flex justify-center md:justify-start items-center">
-              <button className="bg-transparent border border-green-500 w-fit text-green-500 hover:border-green-300 hover:text-green-300 px-4 py-2 rounded-full flex items-center space-x-2 mt-2">
-                <span>Download My CV</span>
-                <span>
-                  {" "}
-                  <Download />
-                </span>
-              </button>
-            </div>
-            <div className="social-media flex items-center justify-center md:justify-start px-3">
-              <a
-                href="#github"
-                className="text-green-500 hover:text-green-300 mr-10"
+    <>
+      <div className="text-white min-h-[88vh] flex flex-col items-center justify-center ubuntu-text px-4">
+        <main className="flex flex-col-reverse  md:flex-row items-center justify-between w-full max-w-6xl">
+          <div className="text-center md:text-left md:w-1/2 space-y-3">
+            <h2 className="text-xl">Full Stack Developer</h2>
+            <h1 className="text-[28px] sm:text-[37px] font-bold ubuntu">
+              Hello I'm <br></br>
+              <div
+                ref={textRef}
+                className="text-green-500 min-h-16 h-auto  w-full"
               >
-                <Github />
-              </a>
-              <a
-                href="#linkedin"
-                className="text-green-500 hover:text-green-300 mr-10"
-              >
-                <Linkedin />
-              </a>
-              <a
-                href="#twitter"
-                className="text-green-500 hover:text-green-300 mr-10"
-              >
-                <Twitter />
-              </a>
-              <a
-                href="#youtube"
-                className="text-green-500 hover:text-green-300"
-              >
-                <Instagram />
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="mt-10 mb-10 md:mt-0 md:w-1/2 flex justify-center">
-          <div className="relative image-wraper flex justify-center items-center  w-full ">
-            <img
-              src={avtarImg}
-              alt="Profile"
-              className="avater-image rounded-full z-40 w-[200px] sm:w-[300px] h-[200px] sm:h-[300px] object-cover"
-            />
-            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-transparent">
-              <PhotoBorder />
+                {" "}
+              </div>
+            </h1>
+            <p className="text-zinc-400 text-wrap">
+              I excel at crafting elegant digital experiences and I am
+              proficient in various programming languages and technologies.
+            </p>
+            <div className="flex flex-col space-y-6 justify-center md:justify-start">
+              <div className="wrapper w-full flex justify-center md:justify-start items-center">
+                <button className="bg-transparent border border-green-500 w-fit text-green-500 hover:border-green-300 hover:text-green-300 px-4 py-2 rounded-full flex items-center space-x-2 mt-2">
+                  <span>Download My CV</span>
+                  <span>
+                    {" "}
+                    <Download />
+                  </span>
+                </button>
+              </div>
+              <div className="social-media flex items-center justify-center md:justify-start ">
+                <a
+                  href="#github"
+                  className="text-green-500 hover:text-green-300 mr-10"
+                >
+                  <Github />
+                </a>
+                <a
+                  href="#linkedin"
+                  className="text-green-500 hover:text-green-300 mr-10"
+                >
+                  <Linkedin />
+                </a>
+                <a
+                  href="#twitter"
+                  className="text-green-500 hover:text-green-300 mr-10"
+                >
+                  <Twitter />
+                </a>
+                <a
+                  href="#youtube"
+                  className="text-green-500 hover:text-green-300"
+                >
+                  <Instagram />
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-      </main>
-      <footer className="w-full mt-8 flex justify-center items-center">
-        <div className="wrap w-full flex flex-wrap justify-between text-center md:text-left md:px-3 md:space-y-0 lg:space-x-4">
-          <div className="flex flex-col items-center md:flex-row md:items-center space-x-2">
-            <h3 className="text-[32px] sm:text-[40px] font-bold">2+</h3>
-            <p className="text-center text-zinc-400 text-wrap max-w-[60%] md:justify-start ">
-              Years of experience
-            </p>
+          <div className="mt-10 mb-10 md:mt-0 md:w-1/2 flex justify-center">
+            <div className="relative image-wraper flex justify-center items-center  w-full ">
+              <img
+                src={avtarImg}
+                alt="Profile"
+                className="avater-image rounded-full z-40 w-[200px] sm:w-[300px] h-[200px] sm:h-[300px] object-cover"
+              />
+              <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-transparent">
+                <PhotoBorder />
+              </div>
+            </div>
           </div>
-          <div className="flex flex-col items-center md:flex-row md:items-center space-x-2">
-            <h3 className="text-[32px] sm:text-[40px] font-bold">15+</h3>
-            <p className="text-center text-zinc-400 text-wrap w-[60%] ">
-              Projects completed
-            </p>
+        </main>
+        <footer className="w-full mt-8 flex justify-center items-center">
+          <div className="wrap w-full grid grid-cols-2 gap-10 sm:grid-cols-4 text-center md:text-left  md:space-y-0 lg:space-x-4 ">
+            <div className="flex flex-col items-center md:flex-row md:items-center space-x-2">
+              <h3 className="text-[32px] sm:text-[40px] font-bold">2+</h3>
+              <p className="text-center text-zinc-400 text-wrap max-w-[60%] md:justify-start ">
+                Years of experience
+              </p>
+            </div>
+            <div className="flex flex-col items-center md:flex-row md:items-center space-x-2">
+              <h3 className="text-[32px] sm:text-[40px] font-bold">15+</h3>
+              <p className="text-center text-zinc-400 text-wrap w-[60%] ">
+                Projects completed
+              </p>
+            </div>
+            <div className="flex flex-col items-center md:flex-row md:items-center space-x-2">
+              <h3 className="text-[32px] sm:text-[40px] font-bold">8+</h3>
+              <p className="text-center text-zinc-400 text-wrap w-[60%] ">
+                Technologies mastered
+              </p>
+            </div>
+            <div className="flex flex-col items-center md:flex-row md:items-center  space-x-2">
+              <h3 className="text-[32px] sm:text-[40px] font-bold">200+</h3>
+              <p className="text-center text-zinc-400  text-wrap w-[40%] ">
+                Code commits
+              </p>
+            </div>
           </div>
-          <div className="flex flex-col items-center md:flex-row md:items-center space-x-2">
-            <h3 className="text-[32px] sm:text-[40px] font-bold">8+</h3>
-            <p className="text-center text-zinc-400 text-wrap w-[60%] ">
-              Technologies mastered
-            </p>
-          </div>
-          <div className="flex flex-col items-center md:flex-row md:items-center  space-x-2">
-            <h3 className="text-[32px] sm:text-[40px] font-bold">200+</h3>
-            <p className="text-center text-zinc-400  text-wrap w-[40%] ">
-              Code commits
-            </p>
-          </div>
-        </div>
-      </footer>
-    </div>
+        </footer>
+      </div>
+      <About />
+    </>
   );
 };
 
